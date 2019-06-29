9d82992aa0a593d7bf5b3018650dc72c7797e044
mischaaf <49269276+mischaaf@users.noreply.github.com>
2019-06-29 20:50:12 +0200

    Update 05_main.js

--- 05_main.js
+++ 05_main.js
@@ -29,7 +29,7 @@ $("document").ready(function() {
         // specification of all deployment information
         deploy: {
             experimentID: "70",
-            serverAppURL: "https://babe-backend.herokuapp.com/api/submit_experiment/70",
+            serverAppURL: "https://babe-backend.herokuapp.com/api/submit_experiment/",
             deployMethod: "directLink",
             contact_email: "mischaaf@uni-osnabrueck.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
