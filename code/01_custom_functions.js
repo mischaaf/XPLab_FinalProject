551f457fdb0e65c4d0df7d6aff4dc2ef980ceb74
annikatheisen <39410718+annikatheisen@users.noreply.github.com>
2019-07-04 10:15:48 +0200

    Add code files

---
+++ code/01_custom_functions.js
@@ -0,0 +1,56 @@
+// generate random participant IDs
+const dec2hex = function(dec) {
+    return ("0" + dec.toString(16)).substr(-2);
+};
+
+// generate participant ID and typecast it into a String
+const generateID = function(len) {
+    let arr = new Uint8Array((len || 40) /2);
+    window.crypto.getRandomValues(arr);
+    return Array.from(arr, this.dec2hex).join("");
+};
+
+// generate array of all stimuli IDs
+var images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+/* to be able to present the stimuli in a randomised order as prescribed in the paper,
+   we shuffle our stimuli array individually for each trial and simply traverse through the
+   shuffled array */
+
+// shuffle stimuli array for first trial, using the default shuffle function
+var array_like = _.shuffle(images);
+
+// shuffle stimuli array for second trial, using the default shuffle function
+var array_object = _.shuffle(images);
+
+// initialise empty array for first trial
+trials_like = [];
+
+/* for loop for first trial:
+   traverses through all randomised stimuli images and presents the participant with
+   a rating scale for each image; pushes information for each picture onto the first trial array to save them */
+for(i=0; i<30; i++) {
+  trials_like.push({
+    picture: "images/" + String(array_like[i]) + ".jpg",
+    question: "  <br /> <br />   <br /> <br /> How much do you like the painting?",
+    optionLeft: 'Not at all',
+    optionRight: 'Very much',
+    picture_nr: array_like[i]
+  }
+)};
+
+// initialise empty array for second trial
+trials_objects = [];
+
+/* for loop for second trial:
+   traverses through all randomised stimuli images and presents the participant with
+   a rating scale for each image; pushes information for each picture onto the second trial array to save them */
+for(i=0; i<30; i++) {
+  trials_objects.push({
+    picture: "images/" + String(array_object[i]) + ".jpg",
+    question: "<br /> <br />   <br /> <br /> How well can you detect objects in the painting?",
+    optionLeft: 'Very hard',
+    optionRight: 'Very easy',
+    picture_nr: array_object[i]
+});
+}
---
+++ code/04_views.js
@@ -0,0 +1,131 @@
+// instantiate intro view to welcome participants and give an introduction to the experiment
+const intro = babeViews.view_generator('intro', {
+    trials: 1,
+    name: 'intro',
+    text:   `Hello dear participant!
+            <br />
+            <br />
+            We welcome you to our experiment.
+            <br />
+            <br />
+            Our experiment is divided into two blocks in which you will see different paintings.
+            In the first block, you are asked to rate how much you like the painting you see.
+            In the second block, you are asked to rate how well you are able to detect objects in the painting.
+            <br />
+            <br />
+            If you're ready, let's go and push the button below!`,
+   buttonText: 'Begin the experiment'
+});
+
+// present instructions to explain the given task for the first block
+const instructions_like = babeViews.view_generator('instructions',{
+    trials: 1,
+    name: 'instructions_like',
+    title: 'Instructions for the first block',
+    text:  `Please read the instructions before starting the experiment.
+            <br />
+            <br />
+            You will see one painting in each trial.
+            You are asked to rate how much you like the painting on a scale from 1 ("not at all") to 7 ("very much").
+            Please stay focused during the experiment.`,
+    buttonText: 'Start the first block'
+});
+
+// present instructions to explain the given task for the second block
+const instructions_object = babeViews.view_generator('instructions',{
+    trials: 1,
+    name: 'instructions_object',
+    title: 'Instructions for the second block',
+    text:  `Please read the instructions before starting the experiment.
+            <br />
+            <br />
+            You will see one painting in each trial.
+            You are asked to rate how well you are able to detect objects in the painting from 1 ("very hard") to 7 ("very easy").`,
+    buttonText: 'Start the second block'
+});
+
+// initialise default post test questionnaire to gain additional information
+const post_test = babeViews.view_generator('post_test',{
+    trials: 1,
+    name: 'post_test',
+    title: 'Additional information',
+    text: 'Please answer the following questions to help us analyse our results.'
+});
+
+// present thanks views and submit results
+const thanks = babeViews.view_generator('thanks',{
+    trials: 1,
+    name: 'thanks',
+    title: 'Thank you for taking part in our experiment!',
+    prolificConfirmText: 'Press the button'
+});
+
+// present desktop distance view to ensure that participants adhere to the mandatory distance of 55cm
+const desktop_distance = babeViews.view_generator('forced_choice',{
+    trials: 1,
+    name: 'desktop_distance',
+    trial_type: 'desktop_distance',
+    data: desktop_distance_trial.desktop_distance,
+});
+
+// initialse question whether participants are experts in cubism
+const expert = babeViews.view_generator('rating_scale',{
+    trials: 1,
+    name: 'expert',
+    trial_type: 'expert',
+    data: rating_scale_question.rating_scale,
+});
+
+// initialise first trial block
+const rating_scale_like = babeViews.view_generator('rating_scale',{
+    trials: trial_info.rating_scale_like.length,
+    name: 'rating_scale_like',
+    trial_type: 'rating_scale_like',
+    data: trial_info.rating_scale_like,
+});
+
+// initialise second trial block
+const rating_scale_object = babeViews.view_generator('rating_scale',{
+    // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
+      trials: trial_info.rating_scale_object.length,
+    // name and trial_type should be identical to the variable name
+      name: 'rating_scale_object',
+      trial_type: 'rating_scale_object',
+      data: trial_info.rating_scale_object,
+});
+
+// present instruction view for the eyesight tests
+const instructions_vision = babeViews.view_generator('instructions',{
+    trials: 1,
+    name: 'instructions_vision',
+    title: 'You have almost finished',
+    text:  `Before the end, we would like to shortly assess your vision and colour vision.
+            <br />
+            <br />
+            If you are wearing glasses or contact lenses at the moment, this is fine. Please keep them on.`,
+    buttonText: 'Check my vision'
+});
+
+// initiate Ishihara colour vision test
+const ishihara = babeViews.view_generator('textbox_input', {
+    trials: 1,
+    name: 'ishihara',
+    trial_type: 'ishihara',
+    data: ishihara_trial.ishihara_test,
+});
+
+// initiate eyesight test
+const vision_test = babeViews.view_generator('textbox_input', {
+  trials: 1,
+  name: 'vision_test',
+  trial_type: 'vision_test',
+  data: vision_test_trial.vision_test,
+});
+
+// iniate second part of eyesight test ???
+const vision_test_2 = babeViews.view_generator('textbox_input', {
+  trials: 1,
+  name: 'vision_test_2',
+  trial_type: 'vision_test_2',
+  data: vision_test_2_trial.vision_test_2,
+});
